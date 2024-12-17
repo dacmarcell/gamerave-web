7be443143766ad64e7482c2c4b23b3bd49ada4e0
dacmarcell <dacmarcell@gmail.com>
2024-12-16 22:58:51 -0300

    Enhance Card component to support clickable links with optional href prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,28 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+
 interface CardProps {
   header: string;
   title: string;
   content: string;
+  href?: string;
   lastUpdated?: string;
 }
 
 function Card(props: CardProps) {
-  const { content, header, lastUpdated, title } = props;
+  const { content, header, lastUpdated, title, href } = props;
+
+  const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+    <div
+      className={`relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 ${
+        href ? "hover:cursor-pointer" : ""
+      }`}
+      {...(href ? { onClick: () => router.push(`${pathname}/${href}`) } : {})}
+    >
       <div className="mx-3 mb-0 border-b border-slate-200 pt-3 pb-2 px-1">
         <span className="text-sm font-medium text-slate-600">{header}</span>
       </div>
