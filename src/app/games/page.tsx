14be0b6bbb2a94edf78a294068669d692d0c0383
dacmarcell <dacmarcell@gmail.com>
2024-12-16 22:29:21 -0300

    Add GamesPage component to fetch and display games

---
+++ src/app/games/page.tsx
@@ -0,0 +1,23 @@
+import GameCard from "@/components/GameCard";
+import { BASE_URL } from "@/constants";
+import { Game } from "@/types";
+
+export async function getGamesAction() {
+  const res = await fetch(`${BASE_URL}/games`);
+  const games: Game[] = await res.json();
+
+  return { games };
+}
+
+async function GamesPage() {
+  const { games } = await getGamesAction();
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {games.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </div>
+  );
+}
+
+export default GamesPage;
