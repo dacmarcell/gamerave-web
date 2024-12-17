5d39ddf9d472f5476f1503200f1d62ed79f2c67a
dacmarcell <dacmarcell@gmail.com>
2024-12-16 22:29:24 -0300

    Add Card component for displaying content with header and last updated info

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+interface CardProps {
+  header: string;
+  title: string;
+  content: string;
+  lastUpdated?: string;
+}
+
+function Card(props: CardProps) {
+  const { content, header, lastUpdated, title } = props;
+
+  return (
+    <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+      <div className="mx-3 mb-0 border-b border-slate-200 pt-3 pb-2 px-1">
+        <span className="text-sm font-medium text-slate-600">{header}</span>
+      </div>
+
+      <div className="p-4">
+        <h5 className="mb-2 text-slate-800 text-xl font-semibold">{title}</h5>
+        <p className="text-slate-600 leading-normal font-light">{content}</p>
+      </div>
+      {lastUpdated ? (
+        <div className="mx-3 border-t border-slate-200 pb-3 pt-2 px-1">
+          <span className="text-sm text-slate-600 font-medium">
+            Last updated: {lastUpdated}
+          </span>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Card;
